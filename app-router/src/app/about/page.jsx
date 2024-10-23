d39acf6f486ccbb369560702748d328cdffbf772
GodMinseok <mseok830@naver.com>
2024-10-23 19:17:17 +0900

    Local과 Remote 방식

--- app-router/src/app/about/page.jsx
+++ app-router/src/app/about/page.jsx
@@ -11,17 +11,29 @@
 // }
 
 import Image from "next/image";
+import foo from "/public/images/leaf-6760484_1920.jpg";
 
 export default function About() {
   return (
     <>
       <h1>About page</h1>
+      {/* 경로 방식 */}
       <Image
-        src="/images/cat-8579018_1280.jpg" // 경로 수정
-        alt="고양이"
-        width={300}
+        src="/images/corn-9064747_640.jpg"
+        alt="옥수수"
+        width={400}
         height={500}
       />
+      <Image
+        src="/images/corn-9064747_640.jpg"
+        alt="옥수수"
+        width={400}
+        height={500}
+        layout="responsive"
+      />
+
+      {/* import 방식 */}
+      <Image src={foo} alt="단풍" width={400} height={500} />
     </>
   );
 }
