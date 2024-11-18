dacdd248021b72ea5b102e70bff330dbe1f6682e
GodMinseok <mseok830@naver.com>
2024-11-18 18:08:36 +0900

    수정 사항 업데이트

---
+++ next14-ui/src/components/ThemeToggleButton.jsx
@@ -0,0 +1,13 @@
+import { ThemeContext } from "@emotion/react";
+import { useContext } from "react";
+
+const ThemeToggleButton = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  return (
+    <button onClick={toggleTheme}>
+      {theme === "light" ? "Dark Mode" : "Light Mode"}
+    </button>
+  );
+};
+
+export default ThemeToggleButton;
---
+++ next14-ui/src/contexts/ThemeContext.jsx
@@ -0,0 +1,43 @@
+// import { useEffect } from "react";
+// import { useState } from "react";
+// import { createContext } from "react";
+
+// const ThemeContext = createContext();
+
+// export const ThemeProvider = ({ children }) => {
+//   const [theme, setTheme] = useState("light");
+
+//   const toffleTheme = () => {
+//     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+//   };
+//   useEffect(() => {
+//     document.body.className = theme
+//   }, [theme])
+//   return (
+//     <ThemeContext.provider value={(theme, )}
+//   )
+// };
+
+import { useEffect, useState, createContext } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
