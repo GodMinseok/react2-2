18705c498cca5dbab4c51fed654558f156317a22
GodMinseok <mseok830@naver.com>
2024-12-04 14:45:56 +0900

    세 번째 커밋

--- final/src/app/about/page.js
+++ final/src/app/about/page.js
@@ -1,9 +1,25 @@
+import Navbar from "../component/navbar";
+
 export default function About() {
   return (
     <>
-      <h1>이름</h1>
-      <h1>학번</h1>
-      <h1>파일 경로</h1>
+      <Navbar>
+        <h1>이름</h1> <Name>서민석</Name>
+        <h1>학번</h1>
+        <No>201930112</No>
+        <h1>파일 경로</h1>
+        <Path>src/app</Path>
+      </Navbar>
     </>
   );
 }
+
+// export default function Foo(props) {
+//     console.log(props);
+//     return (
+//       <>
+//         <h1>About page</h1>
+//         <h1>url: {props.params.url[0]}</h1>
+//       </>
+//     );
+//   }
---
+++ final/src/app/layout.tsx
@@ -0,0 +1,10 @@
+import About from "./about/page";
+
+export default function RootLayout() {
+  // return (
+  // //       <About>
+  // //       </About>
+  // //     </body>
+  // //   </html>
+  // )
+}
